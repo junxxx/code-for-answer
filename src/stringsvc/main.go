e795634d804922cec133bd4a57ac522358d62de9
junxxx <hprjunxxx@gmail.com>
2022-01-12 17:21:30 +0800

    go kit microservice

---
+++ src/stringsvc/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/go-kit/kit/log"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+func main() {
+	logger := log.NewLogfmtLogger(os.Stderr)
+
+	var svc StringService
+	svc = stringService{}
+	svc = loggingMiddleware{logger, svc}
+
+	uppercaseHandlder := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandlder)
+	http.Handle("/count", countHandler)
+	http.ListenAndServe(":8081", nil)
+}
---
+++ src/stringsvc/services.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+// StringService provides operations on strings
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+// + StringService implemetation
+type stringService struct{}
+
+var ErrEmpty = errors.New("Empty string")
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
