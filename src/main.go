c517273eece056c4f1bd6d1132bf01d1668aa2cf
junxxx <hprjunxxx@gmail.com>
2022-02-09 17:24:59 +0800

    metrics

--- src/main.go
+++ src/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+	"time"
+)
 
+var finished chan struct{}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func main() {
+	listener, _ := net.Listen("tcp", "localhost:8080")
+	conn, err := listener.Accept()
+
+	go spinner(100 * time.Microsecond)
+	fmt.Println("main goroutine")
 }
