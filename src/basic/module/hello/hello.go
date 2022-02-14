e707744b7f5e4fa9f535531eaee5c309b09c440f
junxxx <hprjunxxx@gmail.com>
2022-02-14 20:57:19 +0800

    version compatible

--- src/basic/module/hello/hello.go
+++ src/basic/module/hello/hello.go
@@ -10,9 +10,11 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Anna")
+
+	names := []string{"Duo", "Anna", "Peter"}
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
